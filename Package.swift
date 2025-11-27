995259b6c2fa7c943f5a615858b77952bc090345
Alexey Nazarov <alexey.nazarov@ptr.vc>
2025-11-27 20:10:04 +0200

    Release 1.40.0

---
+++ Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = Version("1.40.0")
+
+let gitUrl = "https://raw.githubusercontent.com/SumSubstance/IdensicMobileSDK-iOS-Release/master/\(version)"
+
+enum checksums {
+    static let vi = "04d6e8f4bbcc7d3a12db896e7e40f8bb217e0cccbe0b5d3626f4db86634a2590"
+}
+
+let package = Package(
+    name: "IdensicMobileSDK_VideoIdent",
+    platforms: [
+        .iOS("13.0")
+    ],
+    products: [
+        .library(
+            name: "IdensicMobileSDK_VideoIdent",
+            targets: [
+                "VideoIdentWrapper"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SumSubstance/IdensicMobileSDK-iOS.git", .exact(version)),
+        .package(url: "https://github.com/twilio/twilio-video-ios", "5.8.2" ..< "6.0.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "IdensicMobileSDK_VideoIdent",
+            url: "\(gitUrl)/IdensicMobileSDK_VideoIdent-\(version).zip",
+            checksum: checksums.vi
+        ),
+        .target(
+            name: "VideoIdentWrapper",
+            dependencies: [
+                .product(name: "IdensicMobileSDK", package: "IdensicMobileSDK-iOS"),
+                "IdensicMobileSDK_VideoIdent",
+                .product(name: "TwilioVideo", package: "twilio-video-ios")
+            ],
+            path: "VideoIdentWrapper"
+        )
+    ]
+)
